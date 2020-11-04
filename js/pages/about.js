d6742aa7f9abeffc21d5bf2f10ad148acc4e7fd4
aviv levy <avivlev53@gmail.com>
2020-11-04 13:35:47 +0200

    css

--- js/pages/about.js
+++ js/pages/about.js
@@ -1,7 +1,15 @@
 export default {
     template: `
     <section>
-        <h1>about us..</h1>       
+        <h1>about us..</h1> 
+        <section class="info">
+            <section class="about-aviv">
+                <h1>Aviv Levy</h1> 
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam voluptate, blanditiis deserunt voluptatibus, perspiciatis perferendis dicta labore pariatur ad odio laudantium ipsam maxime. Alias, amet porro! Magni, voluptas voluptate. Quas?</p>
+
+            </section>
+            <section class="about-eilon"></section>
+        </section>      
     </section>
     `
 }
