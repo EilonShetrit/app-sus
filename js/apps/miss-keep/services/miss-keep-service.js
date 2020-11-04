f4f8f618649801349275817b1eae6d997098d44f
Eilon Shetrit <eilonshetrit@gmail.com>
2020-11-04 17:41:13 +0200

    service commit

--- js/apps/miss-keep/services/miss-keep-service.js
+++ js/apps/miss-keep/services/miss-keep-service.js
@@ -1,46 +1,49 @@
-// export const missKeepService = {
-//     getNotes
-// }
+// import {utilService} from '.'
 
-// const notes = [
-//     {
-//         type: "NoteText",
-//         isPinned: true,
-//         info: {
-//             txt: "Fullstack Me Baby!"
-//         }
-//     },
-//     {
-//         type: "NoteImg",
-//         info: {
-//             url: "http://some-img/me",
-//             title: "Me playing Mi"
-//         },
-//         style: {
-//             backgroundColor: "#00d"
-//         }
-//     },
-//     {
-//         type: "NoteTodos",
-//         info: {
-//             label: "How was it:",
-//             todos: [
-//                 {
-//                     txt: "Do that",
-//                     doneAt: null
-//                 },
-//                 {
-//                     txt: "Do this",
-//                     doneAt: 187111111
-//                 }
-//             ]
-//         }
-//     }
-// ];
+export const missKeepService = {
+    getNotes
+}
+
+const notes = [
+    {
+        type: "NoteText",
+        isPinned: true,
+        info: {
+            txt: "Fullstack Me Baby!"
+        }
+    },
+    {
+        type: "NoteImg",
+        info: {
+            url: "http://some-img/me",
+            title: "Me playing Mi"
+        },
+        style: {
+            backgroundColor: "#00d"
+        }
+    },
+    {
+        type: "NoteTodos",
+        info: {
+            label: "How was it:",
+            todos: [
+                {
+                    
+                    txt: "Do that",
+                    doneAt: null
+                },
+                {
+                    txt: "Do this",
+                    doneAt: 187111111
+                }
+            ]
+        }
+    }
+];
 
 
-// function getNotes() {
-//     return Promise.resolve(notes);
-// }
+function getNotes() {
+    return Promise.resolve(notes);
+}
 
 
