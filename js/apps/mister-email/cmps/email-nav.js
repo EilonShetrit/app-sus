2d18f363c6e7ab53ac2cc0e7ec042cc1b29e594b
aviv levy <avivlev53@gmail.com>
2020-11-04 20:46:35 +0200

    wwww

--- js/apps/mister-email/cmps/email-nav.js
+++ js/apps/mister-email/cmps/email-nav.js
@@ -6,8 +6,8 @@ export default {
     <section>
         <nav  class="email-nav flex">
             <router-link to="/email/inbox" exact>inbox</router-link>
-            <router-link>Starred</router-link>
-            <router-link>Sent email</router-link>
+            <router-link to="/email/inbox">Starred</router-link>
+            <router-link to="/email/inbox">Sent email</router-link>
             <!-- <router-link >Drafts</router-link> -->
         </nav>        
     </section>
